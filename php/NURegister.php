22ce5a14776db9d389e3cf08be9db6ca7fe0ef30
Alexpon <ponpon88810@gmail.com>
2015-11-03 19:57:53 +0800

    php ver1.0
    add NURegister.php

---
+++ php/NURegister.php
@@ -0,0 +1,17 @@
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+<?php
+	$con = mysqli_connect("address", "username", "password", "db");
+
+	$name = $_POST["name"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$department = $_POST["department"];
+	$email = $_POST["email"];
+	
+	$statement = mysqli_prepare($con, "INSERT INTO table (username, password, name, department, email) VALUES (?, ?, ?, ?, ?)");
+	mysqli_stmt_bind_param($statement, "sssss", $username, $password, $name, $department, $email);
+	mysqli_stmt_execute($statement);
+	mysqli_stmt_close($statement);
+	mysqli_close($con);
+?>
